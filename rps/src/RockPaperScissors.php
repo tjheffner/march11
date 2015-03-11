a992c05b57f4d87be76f6cf5ca0c91a0b166d44f
Tanner Heffner & John Franti <student@epicodus.com>
2015-03-11 10:08:20 -0700

    Add all tests, pass all tests

--- rps/src/RockPaperScissors.php
+++ rps/src/RockPaperScissors.php
@@ -2,12 +2,32 @@
 
     class RockPaperScissors
     {
-        function shoot()
+        function shoot($input1, $input2)
         {
-            
+            if (($input1 == "rock") && ($input2 == "scissors")) {
+                return "Player 1";
+            }
+            elseif (($input1 == "rock") && ($input2 == "paper")) {
+                return "Player 2";
+            }
+            elseif (($input1 == "paper") && ($input2 == "scissors")) {
+                return "Player 2";
+            }
+            elseif (($input1 == "scissors") && ($input2 == "rock")) {
+                return "Player 2";
+            }
+            elseif (($input1 == "paper") && ($input2 == "rock")) {
+                return "Player 1";
+            }
+            elseif (($input1 == "scissors") && ($input2 == "paper")) {
+                return "Player 1";
+            }
+            else {
+                return "Draw";
+            };
+
         }
 
     }
-
 
 ?>
--- rps/tests/RockPaperScissorsTest.php
+++ rps/tests/RockPaperScissorsTest.php
@@ -18,6 +18,84 @@
             //Assert
             $this->assertEquals("Player 1", $result);
         }
+        function test_shoot_rp()
+        {
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $input1 = "rock";
+            $input2 = "paper";
 
+            //Act
+            $result = $test_RockPaperScissors->shoot($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Player 2", $result);
+        }
+        function test_shoot_ps()
+        {
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $input1 = "paper";
+            $input2 = "scissors";
+
+            //Act
+            $result = $test_RockPaperScissors->shoot($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Player 2", $result);
+        }
+        function test_shoot_sr()
+        {
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $input1 = "scissors";
+            $input2 = "rock";
+
+            //Act
+            $result = $test_RockPaperScissors->shoot($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Player 2", $result);
+        }
+        function test_shoot_pr()
+        {
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $input1 = "paper";
+            $input2 = "rock";
+
+            //Act
+            $result = $test_RockPaperScissors->shoot($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Player 1", $result);
+        }
+        function test_shoot_sp()
+        {
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $input1 = "scissors";
+            $input2 = "paper";
+
+            //Act
+            $result = $test_RockPaperScissors->shoot($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Player 1", $result);
+        }
+        function test_shoot_draw()
+        {
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $input1 = "scissors";
+            $input2 = "scissors";
+
+            //Act
+            $result = $test_RockPaperScissors->shoot($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Draw", $result);
+        }
+        
     }
 ?>
