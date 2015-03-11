afd4d3b35bdcc8529417da41e187cd67b2e4eee0
Tanner Heffner & John Franti <student@epicodus.com>
2015-03-11 14:43:42 -0700

    Add win counter to results page

--- rps/app/app.php
+++ rps/app/app.php
@@ -5,17 +5,20 @@
     session_start();
     if (empty($_SESSION['winner'])) {
         $_SESSION['winner'] = array();
+        $_SESSION['p1count'] = 0;
+        $_SESSION['p2count'] = 0;
     }
 
     $app = new Silex\Application();
 
-    $app['debug']=TRUE;
+    $app['debug'] = true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__."/../views"
     ));
 
     $app->get("/", function() use ($app) {
+        RockPaperScissors::deleteAll();
         return $app['twig']->render('start.twig');
     });
 
@@ -38,7 +41,7 @@
         $results = new RockPaperScissors;
         $winner = $results->shoot($_POST['player1'], $_POST['player2']);
         $results->save();
-        return $app['twig']->render('results.twig', array('newwinner' => $winner));
+        return $app['twig']->render('results.twig', array('newwinner' => $winner, 'p1wins' => $_SESSION['p1count'], 'p2wins' => $_SESSION['p2count']));
     });
 
     return $app;
--- rps/src/RockPaperScissors.php
+++ rps/src/RockPaperScissors.php
@@ -2,19 +2,25 @@
 
     class RockPaperScissors
     {
+
         function shoot($input1, $input2)
         {
             if ((($input1 == "rock") && ($input2 == "scissors")) ||
                     (($input1 == "paper") && ($input2 == "rock")) ||
                     (($input1 == "scissors") && ($input2 == "paper"))) {
+                array_push($_SESSION['winner'], 'player1');
+                $_SESSION['p1count']++;
                 return "Player 1";
             }
             elseif ((($input1 == "paper") && ($input2 == "scissors")) ||
                     (($input1 == "scissors") && ($input2 == "rock")) ||
                     (($input1 == "rock") && ($input2 == "paper"))) {
+                array_push($_SESSION['winner'], 'player2');
+                $_SESSION['p2count']++;
                 return "Player 2";
             }
             else {
+                $_SESSION['winner'] = array();
                 return "No one";
             };
 
@@ -30,6 +36,8 @@
         static function deleteAll()
         {
             $_SESSION['winner'] = array();
+            $p1wins = 0;
+            $p2wins = 0;
         }
     }
 
